9696cb4aa8a35eb5848e2cf21db1813ebebbc34e
none <none>
2024-02-11 15:08:19 +0000

    min ver 29.1

--- dired-e.el
+++ dired-e.el
@@ -5,7 +5,7 @@
 ;; Author: Anoncheg1
 ;; Keywords: matching, dired, isearch
 ;; URL: https://github.com/Anoncheg1/dired-e-mode
-;; Version: 0.0.2
+;; Version: 0.0.3
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This file is not part of GNU Emacs.
