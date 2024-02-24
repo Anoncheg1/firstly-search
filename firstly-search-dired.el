7a425a455524bb6afdae0c052c680cc63a82ff79
none <none>
2024-02-24 23:40:07 +0000

    0.0.9

--- firstly-search-dired.el
+++ firstly-search-dired.el
@@ -5,7 +5,7 @@
 ;; Author: Anoncheg1
 ;; Keywords: matching, dired, isearch
 ;; URL: https://github.com/Anoncheg1/firstly-search
-;; Version: 0.0.8
+;; Version: 0.0.9
 
 ;; This file is not part of GNU Emacs.
 
